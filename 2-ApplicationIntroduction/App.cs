3b01fa989a33f7c16013337c1be3a3099c0428bd
zhubaitian <zhubaitian@gmail.com>
2019-05-31 18:31:21 +0800

    Initialial setup

---
+++ 2-ApplicationIntroduction/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StartApplicationByCode
+{
+    class App
+    {
+        [STAThread]
+
+        static void Main()
+
+        {
+
+            // 定义Application对象作为整个应用程序入口  
+
+            Application app = new Application();
+
+            // 方法一：调用Run方法 ,这种方式跟winform的调用一样
+
+            MainWindow win = new MainWindow();
+
+            app.Run(win);
+        }
+    }
+}
