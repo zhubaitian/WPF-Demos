67a67065d457c5fa696268bc7ce6e49e81733ca3
zhubaitian <zhubaitian@gmail.com>
2019-06-04 13:19:08 +0800

    Use another treeview sample

--- 19-1-TreeView/MainWindow.xaml.cs
+++ 19-1-TreeView/MainWindow.xaml.cs
@@ -15,6 +15,114 @@ using System.Windows.Shapes;
 
 namespace TreeView
 {
+
+    public class League
+    {
+        public League(string name)
+        {
+            _name = name;
+            _divisions = new List<Division>();
+        }
+
+
+        string _name;
+
+        public string Name { get { return _name; } }
+
+        List<Division> _divisions;
+        public List<Division> Divisions { get { return _divisions; } }
+
+    }
+    public class Division
+    {
+        public Division(string name)
+        {
+            _name = name;
+            _teams = new List<Team>();
+
+        }
+
+        string _name;
+
+        public string Name { get { return _name; } }
+
+        List<Team> _teams;
+
+        public List<Team> Teams { get { return _teams; } }
+
+    }
+
+    public class Team
+    {
+        public Team(string name)
+        {
+            _name = name;
+        }
+
+        string _name;
+
+        public string Name { get { return _name; } }
+    }
+
+    public class ListLeagueList : List<League>
+    {
+        public ListLeagueList()
+        {
+            League l;
+            Division d;
+
+            Add(l = new League("League A"));
+            l.Divisions.Add((d = new Division("Division A")));
+            d.Teams.Add(new Team("Team I"));
+            d.Teams.Add(new Team("Team II"));
+            d.Teams.Add(new Team("Team III"));
+            d.Teams.Add(new Team("Team IV"));
+            d.Teams.Add(new Team("Team V"));
+            l.Divisions.Add((d = new Division("Division B")));
+            d.Teams.Add(new Team("Team Blue"));
+            d.Teams.Add(new Team("Team Red"));
+            d.Teams.Add(new Team("Team Yellow"));
+            d.Teams.Add(new Team("Team Green"));
+            d.Teams.Add(new Team("Team Orange"));
+            l.Divisions.Add((d = new Division("Division C")));
+            d.Teams.Add(new Team("Team East"));
+            d.Teams.Add(new Team("Team West"));
+            d.Teams.Add(new Team("Team North"));
+            d.Teams.Add(new Team("Team South"));
+            Add(l = new League("League B"));
+            l.Divisions.Add((d = new Division("Division A")));
+            d.Teams.Add(new Team("Team 1"));
+            d.Teams.Add(new Team("Team 2"));
+            d.Teams.Add(new Team("Team 3"));
+            d.Teams.Add(new Team("Team 4"));
+            d.Teams.Add(new Team("Team 5"));
+            l.Divisions.Add((d = new Division("Division B")));
+            d.Teams.Add(new Team("Team Diamond"));
+            d.Teams.Add(new Team("Team Heart"));
+            d.Teams.Add(new Team("Team Club"));
+            d.Teams.Add(new Team("Team Spade"));
+            l.Divisions.Add((d = new Division("Division C")));
+            d.Teams.Add(new Team("Team Alpha"));
+            d.Teams.Add(new Team("Team Beta"));
+            d.Teams.Add(new Team("Team Gamma"));
+            d.Teams.Add(new Team("Team Delta"));
+            d.Teams.Add(new Team("Team Epsilon"));
+        }
+
+        public League this[string name]
+        {
+            get
+            {
+                foreach (League l in this)
+                    if (l.Name == name)
+                        return l;
+
+                return null;
+            }
+        }
+
+
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -24,59 +132,8 @@ namespace TreeView
         {
             InitializeComponent();
 
-
-
-            Jobs jobs1 = new Jobs();
-
-            jobs1.Add(new Job() { Id = 1, Name = "CEO1" });
-
-            jobs1.Add(new Job() { Id = 2, Name = "CEO2" });
-
-            jobs1.Add(new Job() { Id = 3, Name = "CEO3" });
-
-
-
-            Jobs jobs2 = new Jobs();
-
-            jobs2.Add(new Job() { Id = 1, Name = "CEO1" });
-
-            jobs2.Add(new Job() { Id = 2, Name = "CEO2" });
-
-            jobs2.Add(new Job() { Id = 3, Name = "CEO3" });
-
-
-
-            Dictionary<string, Jobs> allJob = new Dictionary<string, Jobs>();
-
-            allJob.Add("CEO one", jobs1);
-
-            allJob.Add("CEO two", jobs2);
-
-            //dictree.DataContext = allJob;
-            dictree.ItemsSource = allJob;
-            //dictree.ItemsSource = jobs1;
+            ListLeagueList listLeaugeList = new ListLeagueList();
+            treeView.ItemsSource = listLeaugeList;
         }
     }
-
-    public class Job
-
-    {
-
-        public int Id
-
-        { get; set; }
-
-        public string Name
-
-        { get; set; }
-
-        public string Status
-
-        { get; set; }
-
-    }
-
-
-
-    public class Jobs : System.Collections.ObjectModel.ObservableCollection<Job> { }
 }
