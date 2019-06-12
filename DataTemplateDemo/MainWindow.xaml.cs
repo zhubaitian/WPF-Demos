58ea94290904b94e06ef3dfe844f4848ecdf1420
zhubaitian <zhubaitian@gmail.com>
2019-06-12 22:05:27 +0800

    Add DataTemplateDemo and HierarchicalDataTemplateDemo

---
+++ DataTemplateDemo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplateDemo
+{
+    public class Task
+    {
+        public String TaskName { get; set; }
+        public String Description { get; set; }
+        public override string ToString()
+        {
+            return TaskName.ToString();
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public List<Task> Tasks = new List<Task>();
+        
+        private void InitData()
+        {
+            Tasks.Add(new Task { TaskName = "Go Home", Description = "Go home tasks" });
+            Tasks.Add(new Task { TaskName = "Go to school", Description = "Go to school tasks" });
+            listBox1.ItemsSource = Tasks;
+            listBox2.ItemsSource = Tasks;
+            listBox3.ItemsSource = Tasks;
+            listBox4.ItemsSource = Tasks;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitData();
+        }
+    }
+}
